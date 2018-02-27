d20420402f9e260ea551e750e35d472f0be97790
Marcus Hennig <marcus.hennig@d-fine.de>
2018-02-27 16:27:41 +0100

    Added some test class

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,10 +3,11 @@ import React, { Component } from 'react';
 class App extends Component {
 
   render() {
+
+    let x = new MyClass(2);
     return (
-        <h1>Hello world!</h1>
+        <h1>Hello number {x.getResult()+3}</h1>
     );
   }
 }
-
 export default App;
