132a5f2a2c6ba09a43d489ad0c50b4d83d7f04db
marcushennig <35DEv5bq>
2018-03-04 10:44:16 +0100

    Added flow support

--- src/App.js
+++ src/App.js
@@ -1,19 +1,65 @@
-import React, { Component } from 'react';
+
+
+import React, {Component} from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import MyAwesomeReactComponent from './components/MyAwesomeReactComponent';
-import MyClass from './shared/test'
 
-class App extends Component
-{
+//@flow
+function concat(a: string, b: string) {
+  return a + b;
+}
+
+function DatePicker(props) {
+  return(
+    <div>
+      Imagine a {props.color} datepicker here.
+    </div>
+  );
+}
+function Item(props) {
+  return(
+    <li key={props.key}>
+      {props.message}
+    </li>
+  );
+}
+
+function TodoList() {
+  const todos = ['finish doc', 'submit pr', 'nag dan to review'];
+  return (
+    <ul>
+      {todos.map(message => <Item key={message} message={message}/>)}
+    </ul>
+  );
+}
+class TestComponent extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.value
+    }
+  }
 
   render() {
-
-    let x = new MyClass(2);
     return (
-      <MuiThemeProvider>
-        <MyAwesomeReactComponent />
-      </MuiThemeProvider>
+      <div>
+        <button>
+          {this.state.value}
+        </button>
+      </div>
     );
   }
 }
+
+function App() {
+  return (
+    <MuiThemeProvider>
+      <MyAwesomeReactComponent/>
+      <DatePicker color="blue"/>
+      <TestComponent value={"Hello"}/>
+      <TodoList/>
+    </MuiThemeProvider>);
+}
+
 export default App;
